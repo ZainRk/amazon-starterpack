a848c253e364df07ca4e3056b65cb57228e769c2
Kashif Khan <engrmalakkashif@gmail.com>
2023-07-06 09:04:45 +0500

    Hero

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import css from './Hero.module.css'
+const Hero = () => {
+  return (
+    <div className={css.Hero}>
+        
+    </div>
+  )
+};
+
+export default Hero;
