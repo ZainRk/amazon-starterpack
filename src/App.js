dde9837bbee5e1d5aa4a29ea860adad745a75c4b
Kashif Khan <engrmalakkashif@gmail.com>
2023-07-14 22:36:05 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <div className="App">
       <Header /> 
+      <Hero />
     </div>
   );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
 import css from './Hero.module.css'
+import Heroimg from '../../assets/hero.png'
+import {RiShoppingBagFill} from 'react-icons/ri';
+import {BsArrowRight} from 'react-icons/bs';
+
+
+
 const Hero = () => {
   return (
-    <div className={css.Hero}>
-        
+    <div className={css.container}>
+        {/* Left Side */}
+        <div className={css.h_sides}>
+        <span className={css.text1}> Skin Protection Cream </span>
+        <div className={css.text2}>
+          <span>Trendy Collection</span>
+          <span> Seedily Say has suitible disposal and boy rejoiced</span>
+        </div>
+        </div>
+        {/* middle side  hero image */}
+        <div className={css.wrapper}>
+          <div className={css.bluecircle}>
+            <img src={Heroimg} alt="" width={600} />
+            <div className={css.cart2}>
+            <RiShoppingBagFill />
+            <div className={css.signup}>
+              <span>Best sign of offers</span>
+            </div>
+            <BsArrowRight/>
+            </div>
+          </div>
+        </div>
+        {/* Right Side */}
+        <div className={css.h_sides}>
+          <div className={css.traffic}>
+            <span>1.5m</span>
+            <span>Monthly Traffic</span>
+          </div>
+        <div className={css.customers}>
+            <span>100k</span>
+            <span>Happy Customers</span>
+        </div>
+        </div>
     </div>
   )
 };
